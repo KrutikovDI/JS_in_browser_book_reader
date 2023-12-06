1ca70695aa89d0fc26cb94e926a24bbd0a415360
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-06 20:23:25 +0700

    Second commit

--- task.js
+++ task.js
@@ -2,28 +2,38 @@ const fontSizes = document.querySelector(".book__control");
 const fontSize = [...fontSizes.children];
 const bookContent = document.querySelector(".book__content");
 fontSizes.addEventListener("click", (e) => {
-  removeClass(fontSize, "font-size_active");
-  e.preventDefault();
-  e.target.classList.add("font-size_active");
-  deleteClass(bookContent, "font-size_big", "font-size_small");
-  console.log(bookContent.getAttribute("class"));
-  if (e.target.getAttribute("data-size") == "big") {
-    bookContent.classList.add("font-size_big");
-  } else if (e.target.getAttribute("data-size") == "small") {
-    bookContent.classList.add("font-size_small");
+  if (e.target.tagName == "DIV") {
+    e.preventDefault()
+  }
+  else {
+    removeClass(fontSize, "font-size_active");
+    e.preventDefault();
+    e.target.classList.add("font-size_active");
+    deleteClass(bookContent, "font-size_big", "font-size_small");
+    if (e.target.getAttribute("data-size") == "big") {
+      bookContent.classList.add("font-size_big");
+    } else if (e.target.getAttribute("data-size") == "small") {
+      bookContent.classList.add("font-size_small");
+    }
   }
 });
- 
-
 const colorText = document.querySelector(".book__control.book__control_color");
 const colorTextArray = [...colorText.children];
 colorTextArray.splice(0, 1);
 colorText.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (e.target.tagName != "SPAN") {
+  if ((e.target.className == "color__title") || (e.target.className == "book__control book__control_color")) {
+    e.preventDefault();
+  }
+  else {
+    e.preventDefault();
     removeClass(colorTextArray, "color_active");
     e.target.classList.add("color_active");
-    deleteClass(bookContent, "book_color-black", "book_color-gray", "book_color-whitesmoke");
+    deleteClass(
+      bookContent,
+      "book_color-black",
+      "book_color-gray",
+      "book_color-whitesmoke"
+    );
     if (e.target.getAttribute("data-text-color") == "black") {
       e.target.classList.add("book_color-black");
     } else if (e.target.getAttribute("data-text-color") == "gray") {
@@ -33,28 +43,35 @@ colorText.addEventListener("click", (e) => {
     }
   }
 });
- 
 const colorbg = document.querySelector(
-  ".book__control.book__control_background");
+  ".book__control.book__control_background"
+);
 let colorbgArray = [...colorbg.children];
 colorbgArray.splice(0, 1);
 colorbg.addEventListener("click", (e) => {
   e.preventDefault();
   deleteClass();
-  if (e.target.tagName != "SPAN") {
-    removeClass(colorbgArray, "color_active");
-    e.target.classList.add("color_active");
-    deleteClass(bookContent, "bg_color_black", "bg_color_gray", "bg_color_white");
-    if (e.target.getAttribute("data-bg-color") == "black") {
-      bookContent.classList.add("bg_color_black");
-    } else if (e.target.getAttribute("data-bg-color") == "gray") {
-      bookContent.classList.add("bg_color_gray");
-    } else if (e.target.getAttribute("data-bg-color") == "white") {
-      bookContent.classList.add("bg_color_white");
-    }
+  if ((e.target.className == "color__title") || (e.target.className == "book__control book__control_background")) {
+    e.preventDefault()}
+  else {
+      removeClass(colorbgArray, "color_active");
+      e.target.classList.add("color_active");
+      deleteClass(
+        bookContent,
+        "bg_color_black",
+        "bg_color_gray",
+        "bg_color_white"
+      );
+      if (e.target.getAttribute("data-bg-color") == "black") {
+        bookContent.classList.add("bg_color_black");
+      } else if (e.target.getAttribute("data-bg-color") == "gray") {
+        bookContent.classList.add("bg_color_gray");
+      } else if (e.target.getAttribute("data-bg-color") == "white") {
+        bookContent.classList.add("bg_color_white");
+      }
   }
 });
- 
+
 // функция по обнулению активного класса
 function removeClass(array, className) {
   for (i = 0; i < array.length; i++) {
@@ -63,7 +80,7 @@ function removeClass(array, className) {
     }
   }
 }
- 
+
 // функция по удалеию ненужных классов
 function deleteClass(element, ...classes) {
   for (let i = 0; i < classes.length; i++) {
